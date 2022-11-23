1e3e6864b1b78d5fb9895cc2965dd651f59ded0a
Anton <104712265+m6129@users.noreply.github.com>
2022-11-23 14:11:16 +0300

    Update te.py

--- te.py
+++ te.py
@@ -2,9 +2,7 @@ import io # обязательные библиотеки для stremlit
 import streamlit as st # # обязательные библиотеки для stremlit
 from PIL import Image # библиотека для загрузки изображений
 #import torch
-from transformers import SegformerFeatureExtractor, SegformerForSemanticSegmentation
-from transformers import pipeline
-import requests
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 
 #@st.cache(allow_output_mutation=True)
 #def load_model():
@@ -27,10 +25,13 @@ result = st.button('Распознать изображение')# вставл�
 st.write('**Успешно3:**')
 if result: #после нажатия на которую будет запущен алгоритм...
     st.write('**Результаты распознавания:**')
-    feature_extractor = SegformerFeatureExtractor.from_pretrained("nvidia/segformer-b5-finetuned-ade-512-512")
-    model = SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b5-finetuned-ade-512-512")
-    url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-    image = Image.open(requests.get(url, stream=True).raw)
-    inputs = feature_extractor(images=image, return_tensors="pt")
-    outputs = model(**inputs)
-    logits = outputs.logits  # shape (batch_size, num_labels, height/4, width/4)
+    model_name = "deepset/roberta-base-squad2"
+    # a) Get predictions
+    nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+    QA_input = {'question': 'Why is model conversion important?',
+    'context': 'The option to convert models between FARM and transformers gives freedom to the user and let people easily switch between frameworks.'
+}
+    res = nlp(QA_input)
+    # b) Load model & tokenizer
+    model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
